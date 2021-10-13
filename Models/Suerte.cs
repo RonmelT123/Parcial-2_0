098f362f1423994a128ae6c335945f0818b08b8e
RonmelT123 <a2019210835@estudiantes.upsa.edu.bo>
2021-10-13 19:18:06 -0400

    crear modelo

---
+++ Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parical_2.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public string SuerteID  { get; set; }
+        [Required]
+        public string Detalle { get; set; }
+        [Required(AllowEmptyStrings =false)]
+        [Url]
+      
+        public string Imagen { get; set; }
+
+    }
+}
